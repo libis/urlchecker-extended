829978efabf86a89634ccac67b00fe65b871f18e
Seth Linn <117631186+sethlinnkuleuven@users.noreply.github.com>
2023-07-25 17:42:02 +0200

    Update slack.go

--- pkg/slack/slack.go
+++ pkg/slack/slack.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strings"
 
 	"github.com/bythepixel/urlchecker/pkg/config"
 )
@@ -18,18 +19,37 @@ type SlackWebhookPayload struct {
 	Text string `json:"text"`
 }
 
+// Message represents the status, url and message fields to send a webhook.
+type Message struct {
+	Status  int
+	Url     string
+	Message string
+}
+
 // SlackClient contains the Webhook URL.
 type SlackClient struct {
 	Webhook string
 }
 
-// SendMessage creates a SlackWebhookPayload and sends it to the Webhook URL.
-func (c SlackClient) SendMessage(status int, url string, message string) {
+// SendMessage creates a SlackWebhookPayload from the concatenated messages
+// and sends it to the Webhook URL.
+func (c SlackClient) SendMessage(messages []Message) {
 	repo := os.Getenv(config.EnvGithubRepo)
-	msg := fmt.Sprintf("Repository: %s URL: %s Message: %s", repo, url, message)
+
+	// prepare a slice to hold all formatted messages
+	var formattedMessages []string
+
+	// loop over messages and format them
+	for _, message := range messages {
+		msg := fmt.Sprintf("Status: %d, URL: %s, Message: %s", message.Status, message.Url, message.Message)
+		formattedMessages = append(formattedMessages, msg)
+	}
+
+	// concatenate all formatted messages
+	allMessages := fmt.Sprintf("Repository: %s, Messages: [%s]", repo, strings.Join(formattedMessages, "; "))
 
 	pl := SlackWebhookPayload{
-		Text: msg,
+		Text: allMessages,
 	}
 
 	jsonPayload, _ := json.Marshal(pl)

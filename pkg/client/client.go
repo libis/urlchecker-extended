5ef078589e873c6bf7697762068a4d45dbb6e142
Seth Linn <seth.linn@kuleuven.be>
2023-08-29 19:09:54 +0200

    Update client.go

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -10,25 +10,36 @@ import (
 // Fetch fetches a URL and returns information about the response.
 func Fetch(url string) (int, string, error) {
 
-	//resp, err := http.Get(url)
-
 	tr := &http.Transport{
 		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
 	}
+
+	//client := &http.Client{
+	//	Transport: tr,
+	//	Timeout:   time.Second * 30,
+	//}
+
+	//defer client.CloseIdleConnections() //Cleanup old connections
+	//resp, err := client.Get(url)
 
 	client := &http.Client{
 		Transport: tr,
 		Timeout:   time.Second * 30,
 	}
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return 0, "", err
+	}
 
-	resp, err := client.Get(url)
+	req.Header.Add("Connection", "close")
+	resp, err := client.Do(req)
 
 	if err != nil {
 		return 0, "", err
 	}
 
 	// Cleanup
-	defer client.CloseIdleConnections()
+
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)

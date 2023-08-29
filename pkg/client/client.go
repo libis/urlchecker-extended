bbcab01643ea7dc77d7576f31c89725f84192373
Seth Linn <seth.linn@kuleuven.be>
2023-08-29 15:33:19 +0200

    Update client.go

--- pkg/client/client.go
+++ pkg/client/client.go
@@ -1,31 +1,44 @@
 package client
 
 import (
+	"crypto/tls"
 	"io/ioutil"
 	"net/http"
+	"time"
 )
 
 // Fetch fetches a URL and returns information about the response.
 func Fetch(url string) (int, string, error) {
 
-	resp, err := http.Get(url)
+	//resp, err := http.Get(url)
 
 	//var netClient = &http.Client{
 	//	Timeout: time.Second * 100,
 	//}
 	//resp, err := netClient.Get(url)
 
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+
+	client := &http.Client{
+		Transport: tr,
+		Timeout:   time.Second * 10,
+	}
+
+	resp, err := client.Get(url)
+
 	if resp != nil {
 		defer resp.Body.Close()
 	}
 
 	if err != nil {
-		return 1, "", err
+		return 0, "", err
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return 2, "", err
+		return 0, "", err
 	}
 
 	return resp.StatusCode, string(body), nil

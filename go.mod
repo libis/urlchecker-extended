c3a2746d1a41b1515864b4af9db4d484bbbc350b
Seth Linn <seth.linn@kuleuven.be>
2023-09-15 16:19:55 +0200

    Removed the use of http.Client
    http.Client should not be used in a loop.

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/libis/urlchecker-extended
 
-go 1.17
+go 1.21
--- pkg/client/client.go
+++ pkg/client/client.go
@@ -4,25 +4,14 @@ import (
 	"crypto/tls"
 	"io/ioutil"
 	"net/http"
-	"time"
 )
 
 // Fetch fetches a URL and returns information about the response.
 func Fetch(url string) (int, string, error) {
 
-	// This allows invalid certs
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
-	}
-
-	//Set timeout
-	client := &http.Client{
-		Transport: tr,
-		Timeout:   time.Second * 100,
-	}
-
-	resp, err := client.Get(url)
-
+	// Ignore tls verification
+	http.DefaultTransport.(*http.Transport).TLSClientConfig = &tls.Config{InsecureSkipVerify: true}
+	resp, err := http.Get(url)
 	if err != nil {
 		return 0, "", err
 	}
